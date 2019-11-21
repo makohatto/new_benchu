f662ee203061c41bd1cb1367dcf1be2ec18aa1d7
makohatto <mako480817@gmail.com>
2019-11-21 13:12:25 +0900

    coding is added

--- main.php
+++ main.php
@@ -8,11 +8,15 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>メイン</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+    <h1>メイン画面</h1>
+    <!--ユーザーIDにHTMLタグが含まれても良いようにエスケープする-->
+    <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["NAME"],ENT_QUOTES); ?></u>さん</p>
+    <ul>
+      <li><a href="Logout.php">ログアウト</a></li>
+    </ul>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
